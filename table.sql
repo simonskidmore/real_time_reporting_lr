6edfeea24c21ce0b3e5aa2a45bf3d079adc16440
Simon Skidmore <simon.skidmore@landregistry.gsi.gov.uk>
2014-11-06 13:27:56 +0000

    Updated SQL

--- table.sql
+++ table.sql
@@ -8,10 +8,15 @@ CREATE TABLE rt_stats
   team character varying,
   appn_type character varying,
   hour integer,
-  appn_count integer
+  appn_count integer,
+  units integer
 )
 WITH (
   OIDS=FALSE
 );
 ALTER TABLE rt_stats
   OWNER TO postgres;
+
+  
+  
+  alter user postgres with password 'admin'
