1cd6a28c22e7b9acb987cdda32f42a01f9b317c4
Andrew Moore <mooreandrewlr@gmail.com>
2014-11-06 13:38:21 +0000

    Added ERB Values

--- stats.rb
+++ stats.rb
@@ -3,10 +3,16 @@ require 'erb'
 require 'pg'
 set :bind, '0.0.0.0'
 
-get '/stats' do
+get '/stats/:team' do
+
+  team = params[:team]
 
 conn = PGconn.connect("localhost", "5432", "", "", "postgres", "postgres", "admin")
 res = conn.exec("select * from rt_stats")
+
+
+  @team = team
+  @unitsmarkedoff = '99.9'
 
   erb :stats
 end
