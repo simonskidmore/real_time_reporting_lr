20c8836fc739edb761af9a687abe44050cd6edff
Simon Skidmore <simon.skidmore@landregistry.gsi.gov.uk>
2014-11-06 14:54:37 +0000

    Added appn type to random

--- generate.rb
+++ generate.rb
@@ -17,6 +17,24 @@ while true do
     res = conn.exec("insert into rt_stats (office, team, appn_type, hour, appn_count, units, modate) values ('PH', '#{teams[i]}', 'DLG', null, '1', '#{rand(0.1..0.8).round(2)}', current_date)")
 
   end
+  
+  for i in 0..(teams.length-1)
+    puts teams[i]
+    res = conn.exec("insert into rt_stats (office, team, appn_type, hour, appn_count, units, modate) values ('PH', '#{teams[i]}', 'FR', null, '1', '#{rand(0.1..0.8).round(2)}', current_date)")
+
+  end
+  
+  for i in 0..(teams.length-1)
+    puts teams[i]
+    res = conn.exec("insert into rt_stats (office, team, appn_type, hour, appn_count, units, modate) values ('PH', '#{teams[i]}', 'DFL', null, '1', '#{rand(0.1..0.8).round(2)}', current_date)")
+
+  end
+  
+  for i in 0..(teams.length-1)
+    puts teams[i]
+    res = conn.exec("insert into rt_stats (office, team, appn_type, hour, appn_count, units, modate) values ('PH', '#{teams[i]}', 'TP', null, '1', '#{rand(0.1..0.8).round(2)}', current_date)")
+
+  end
   sleep(5)
 
 end
