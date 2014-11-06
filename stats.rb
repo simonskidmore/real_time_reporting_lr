5a05a6f5a71a2503e5aa7f6edbb0243fa147e9f7
Simon Skidmore <simon.skidmore@landregistry.gsi.gov.uk>
2014-11-06 14:40:36 +0000

    Added Office Parameter

--- stats.rb
+++ stats.rb
@@ -3,7 +3,7 @@ require 'erb'
 require 'pg'
 set :bind, '0.0.0.0'
 
-get '/stats/:team' do
+get '/stats/:office/:team' do
 
   team = params[:team]
 
@@ -18,6 +18,30 @@ conn = PGconn.connect("localhost", "5432", "", "", "postgres", "postgres", "admi
   @unitsmarkedoff = res[0]["sum"]
 
   @team = team
+  
+   @date = Time.now.strftime("%d/%m/%Y")
 
   erb :stats
+ 
+ end
+  
+  get '/stats/:office' do
+
+  office = params[:office]
+
+conn = PGconn.connect("localhost", "5432", "", "", "postgres", "postgres", "admin")
+  res = conn.exec("
+  	select sum(units)
+  	from rt_stats
+  	where modate = current_date
+	and office = '#{office}'
+	")
+
+  @unitsmarkedoff = res[0]["sum"]
+
+  @team = office
+
+  @date = Time.now.strftime("%d/%m/%Y")
+  erb :stats
+  
 end
